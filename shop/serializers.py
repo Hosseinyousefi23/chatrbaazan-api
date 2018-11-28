9abdc59f5121aa881bafb29d5e16f4bf88d2caa5
chavoshipor <mohammad.chavoshipor@gmail.com>
2018-11-28 13:45:19 +0330

    complete response products api

--- shop/serializers.py
+++ shop/serializers.py
@@ -13,7 +13,7 @@ from rest_framework import serializers
 
 from rest_framework_jwt.settings import api_settings
 
-from shop.models import City, Banner, Category, Product, Discount
+from shop.models import City, Banner, Category, Product, Discount, Company, ProductLabel
 from accounts.models import User
 import re
 
@@ -28,7 +28,12 @@ def validate_mobile(mobile):
 class CitySerializer(serializers.ModelSerializer):
     class Meta:
         model = City
-        fields = ('id', 'name', 'english_name')
+        fields = ('id', 'name', 'english_name', 'available')
+
+    def __init__(self, instance, pop=[], *args, **kwargs):
+        super().__init__(instance, **kwargs)
+        for fd in pop:
+            self.fields.pop(fd)
 
 
 class BannerSerializer(serializers.ModelSerializer):
@@ -42,6 +47,12 @@ class BannerSerializer(serializers.ModelSerializer):
         if obj.image:
             return obj.image.url
 
+    def __init__(self, instance, pop=[], *args, **kwargs):
+        super().__init__(instance, **kwargs)
+
+        for fd in pop:
+            self.fields.pop(fd)
+
 
 class CategorySerializer(serializers.ModelSerializer):
     open_chatrbazi = serializers.SerializerMethodField()
@@ -49,7 +60,43 @@ class CategorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Category
-        fields = ('id', 'english_name', 'available')
+        fields = ('id', 'english_name', 'available', 'all_chatrbazi', 'open_chatrbazi')
+
+    def __init__(self, instance, pop=[], *args, **kwargs):
+        super().__init__(instance, **kwargs)
+
+        for fd in pop:
+            self.fields.pop(fd)
+
+    def get_all_chatrbazi(self, obj):
+        pass
+
+    def get_open_chatrbazi(self, obj):
+        pass
+
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = ('name', 'available')
+
+    def __init__(self, instance, pop=[], *args, **kwargs):
+        super().__init__(instance, **kwargs)
+
+        for fd in pop:
+            self.fields.pop(fd)
+
+
+class ProductLabelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductLabel
+        fields = ('name', 'available')
+
+    def __init__(self, instance, pop=[], *args, **kwargs):
+        super().__init__(instance, **kwargs)
+
+        for fd in pop:
+            self.fields.pop(fd)
 
 
 class DiscountSerializer(serializers.ModelSerializer):
@@ -57,16 +104,48 @@ class DiscountSerializer(serializers.ModelSerializer):
         model = Discount
         fields = ('id', 'discount', 'available')
 
+    def __init__(self, instance, pop=[], *args, **kwargs):
+        super().__init__(instance, **kwargs)
+
+        for fd in pop:
+            self.fields.pop(fd)
+
 
 class ProductSerializer(serializers.ModelSerializer):
     image = serializers.SerializerMethodField()
+    company = serializers.SerializerMethodField()
+    category = serializers.SerializerMethodField()
+    label = serializers.SerializerMethodField()
+    city = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
         fields = (
             'name', 'priority', 'explanation', 'expiration_date', 'price', 'chatrbazi', 'is_free', 'english_name',
-            'image')
+            'image', 'category', 'label', 'city', 'company')
 
     def get_image(self, obj):
         if obj.image:
             return obj.image.url
+
+    def get_city(self, obj):
+        if obj.city:
+            return CitySerializer(obj.city.all(), many=True, pop=['available']).data
+
+    def get_company(self, obj):
+        if obj.company:
+            return CompanySerializer(obj.company.all(), many=True, pop=['available']).data
+
+    def get_category(self, obj):
+        if obj.category:
+            return CategorySerializer(obj.category.all(), many=True, pop=['available']).data
+
+    def get_label(self, obj):
+        if obj.label:
+            return ProductLabelSerializer(obj.label.all(), many=True, pop=['available']).data
+
+    def __init__(self, instance, pop=[], *args, **kwargs):
+        super().__init__(instance, **kwargs)
+
+        for fd in pop:
+            self.fields.pop(fd)
