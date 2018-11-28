5674c848a3acac26937c6960f2c7337c9e7fd43f
chavoshipor <mohammad.chavoshipor@gmail.com>
2018-11-28 10:32:19 +0330

    render data category and limit data banner and sorted by id and commit migrations

--- shop/serializers.py
+++ shop/serializers.py
@@ -14,13 +14,8 @@ from rest_framework import serializers
 from rest_framework_jwt.settings import api_settings
 
 from shop.adapters import CustomUserAccountAdapter
-from shop.models import City, Banner, User
+from shop.models import City, Banner, User, Category
 
-User = get_user_model()
-jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
-jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
-jwt_decode_handler = api_settings.JWT_DECODE_HANDLER
-jwt_get_mobile_from_payload = api_settings.JWT_PAYLOAD_GET_USERNAME_HANDLER
 import re
 
 
@@ -100,3 +95,12 @@ class CustomUserDetailsSerializer(serializers.ModelSerializer):
         model = User
         fields = ('email',)
         read_only_fields = ('email',)
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    open_chatrbazi = serializers.SerializerMethodField()
+    all_chatrbazi = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Category
+        fields = ('id', 'english_name', 'available')
