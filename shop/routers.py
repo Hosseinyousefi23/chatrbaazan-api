985b01d4d16015d87fd6e25a08700a649f4cc7cf
chavoshipor <mohammad.chavoshipor@gmail.com>
2018-12-21 11:51:58 +0330

    add:complete model company and revers uri banner and dynamic uri

--- shop/routers.py
+++ shop/routers.py
@@ -28,7 +28,7 @@ urlpatterns = [
     url(r'^city/$', views.GetCity.as_view()),
     url(r'^banner/$', views.GetBanner.as_view()),
     url(r'^user/product/$', views.GetUserProduct.as_view()),
-    url(r'^offer/$', views.GetOffers.as_view()),
-    url(r'^offer/(?P<slug>.*)/$', views.GetOffer.as_view()),
+    url(r'^offer/$', views.GetOffers.as_view(), name="getOffers"),
+    url(r'^offer/(?P<slug>.*)/$', views.GetOffer.as_view(), name="getOffer"),
     url(r'^route/', include(router.urls)),
 ]
