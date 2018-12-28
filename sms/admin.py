191cc61191b2581c6b9c9c9b6bf86b5877533cc6
chavoshipor <mohammad.chavoshipor@gmail.com>
2018-12-28 23:14:12 +0330

    add:list display admin sms user

--- sms/admin.py
+++ sms/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from sms.models import Sms, SmsLog, SmsUser
 
+
+class SmsUserAdmin(admin.ModelAdmin):
+    list_display = ('phone', 'status')
+
+
 admin.site.register(Sms)
 admin.site.register(SmsLog)
-admin.site.register(SmsUser)
+admin.site.register(SmsUser, SmsUserAdmin)
