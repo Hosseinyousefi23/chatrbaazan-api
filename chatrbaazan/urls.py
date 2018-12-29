16d66b5db150b0499c60ca7aca8ba50f22139f98
chavoshipor <mohammad.chavoshipor@gmail.com>
2018-12-29 10:57:09 +0330

    add:confirm email

--- chatrbaazan/urls.py
+++ chatrbaazan/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path
 from django.conf.urls import url, include
-from rest_auth.registration.views import VerifyEmailView
+from rest_auth.registration.views import VerifyEmailView, RegisterView
 from rest_auth.views import LogoutView, PasswordResetView, PasswordResetConfirmView
 from django.conf.urls.static import static
 from allauth.account.views import confirm_email as allauthemailconfirmation
+from allauth.account.views import confirm_email
 
 from rest_framework import routers
 from rest_framework_jwt.views import refresh_jwt_token, ObtainJSONWebToken
@@ -39,10 +40,10 @@ urlpatterns = [
     url(r'^', include('shop.urls')),
     path('auth/login/', ObtainJSONWebToken.as_view()),
     path('auth/registration/', include('rest_auth.registration.urls')),
-    re_path(r'^account-confirm-email/', VerifyEmailView.as_view(),
+    re_path(r'^auth/account-confirm-email/', VerifyEmailView.as_view(),
             name='account_email_verification_sent'),
-    url(r'^rest-auth/registration/account-confirm-email/(?P<key>\w+)/$', allauthemailconfirmation,
-        name="account_confirm_email"),
+    re_path(r'^auth/account-confirm-email/(?P<key>[-:\w]+)/$', VerifyEmailView.as_view(),
+            name='account_confirm_email'),
     path('auth/refresh/', refresh_jwt_token),
     path('auth/verify/', verify_jwt_token),
     url(r'^auth/password/reset/$', PasswordResetView.as_view(),
