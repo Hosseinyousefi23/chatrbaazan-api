acec389b8bd159bf5358a16430abf93dadd6f9c4
mohammadhossein <mh.sh7676@gmail.com>
2018-12-29 10:01:38 -0500

    Update serializers.py

--- contact/serializers.py
+++ contact/serializers.py
@@ -7,7 +7,7 @@ from shop.models import validate_mobile, validate_phone
 
 class ContactSerializer(serializers.ModelSerializer):
     name = serializers.CharField(max_length=300, required=True)
-    family = serializers.CharField(max_length=350, required=True)
+    family = serializers.CharField(max_length=350, required=False)
     phone = serializers.CharField(max_length=14, required=False, validators=[validate_phone])
     mobile = serializers.CharField(max_length=14, required=False, validators=[validate_mobile])
     email = serializers.CharField(max_length=50, required=True)
