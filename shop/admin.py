6d1d6fdf8825cb5962f71de03bcf4b1f2e93422a
chavoshipor <mohammad.chavoshipor@gmail.com>
2019-01-01 15:49:08 +0330

    add:show image for all model app

--- shop/admin.py
+++ shop/admin.py
@@ -20,24 +20,57 @@ class ProductAdmin(admin.ModelAdmin):
     list_filter = ('type' ,)
     search_fields = ('name' , 'category__name' , 'company__name' , 'label__name')
 
+    def headshot_image(self , obj):
+        return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
+            url=obj.image.url ,
+            width=150 ,
+            height=150 ,
+        )
+        )
+
+    readonly_fields = ('headshot_image' ,)
+
 
 class CompanyAdmin(admin.ModelAdmin):
     search_fields = ('name' ,)
 
+    def headshot_image(self , obj):
+        return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
+            url=obj.image.url ,
+            width=150 ,
+            height=150 ,
+        )
+        )
+
+    readonly_fields = ('headshot_image' ,)
+
 
 class GalleryAdmin(admin.ModelAdmin):
-    def headshot_image(self, obj):
+    def headshot_image(self , obj):
         return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
-            url = obj.image.url,
-            width=150,
-            height=150,
+            url=obj.image.url ,
+            width=150 ,
+            height=150 ,
         )
         )
-    readonly_fields = ('headshot_image',)
+
+    readonly_fields = ('headshot_image' ,)
+
+
+class BannerAdmin(admin.ModelAdmin):
+    def headshot_image(self , obj):
+        return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
+            url=obj.image.url ,
+            width=150 ,
+            height=150 ,
+        )
+        )
+
+    readonly_fields = ('headshot_image' ,)
 
 
 admin.site.register(City)
-admin.site.register(Banner)
+admin.site.register(Banner , BannerAdmin)
 admin.site.register(Product , ProductAdmin)
 admin.site.register(UserProduct , UserProductAdmin)
 admin.site.register(Category , CategoryAdmin)
