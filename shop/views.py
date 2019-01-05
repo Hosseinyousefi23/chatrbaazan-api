fd017d5fe9fc7a91920369f813feb31288dba4c2
chavoshipor <mohammad.chavoshipor@gmail.com>
2019-01-05 17:50:19 +0330

    return product not expire

--- shop/views.py
+++ shop/views.py
@@ -352,8 +352,8 @@ class LabelViews(APIView , PageNumberPagination):
                     return None
         if type_product is not None:
             products = products.filter(type=type_product)
-        if company is None:
-            products = products.filter(
+        
+        products = products.filter(
                 Q(expiration_date__gt=datetime.now()) | Q(expiration_date__isnull=True))
 
         return self.paginate_queryset(products , self.request)
