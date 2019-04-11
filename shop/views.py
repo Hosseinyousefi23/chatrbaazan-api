e73ea55cb24e25025f693379481bcff2b382613f
hossein <hossein.yousefi23@gmail.com>
2019-04-11 18:23:51 +0430

    test commit

--- shop/views.py
+++ shop/views.py
@@ -32,7 +32,7 @@ class TestAPi(APIView):
 
     # renderer_classes = (JSONRenderer,)
     def get(self, request, format=None, ):
-        return Response("I Love You Python :) * Hossein *")
+        return Response("I Love You Python :) * Hossein yousefis *")
         # return HttpResponse(uuid.uuid1(random.randint(0, 281474976710655)))
 
 
