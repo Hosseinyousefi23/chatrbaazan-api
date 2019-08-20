4af7cdcdab090a6bce25c21b6fefc7ea27d305b6
hossein <hossein.yousefi23@gmail.com>
2019-08-20 12:22:49 +0430

    activate timezone in django admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.utils import timezone
 # Register your models here.
 from django.utils.safestring import mark_safe
 
+from chatrbaazan.settings import TIME_ZONE
 from shop.models import City, Banner, ShopSetting, Product, Category, Company, ProductLabel, ProductGallery, UserProduct
 
 
@@ -17,6 +19,10 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'failure', 'click', 'count', 'chatrbazi', 'is_free')
     list_filter = ('type',)
     search_fields = ('name', 'category__name', 'company__name', 'label__name')
+
+    def __init__(self, model, admin_site):
+        super().__init__(model, admin_site)
+        timezone.activate(TIME_ZONE)
 
     def headshot_image(self, obj):
         return mark_safe('<img src="{url***REMOVED***" width="{width***REMOVED***" height={height***REMOVED*** />'.format(
